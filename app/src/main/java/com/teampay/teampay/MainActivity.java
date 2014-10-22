3e74e05f6a509c1b07a1f22bd64d80a7749c1a57
sdeniss <sirovdenis@gmail.com>
2014-10-22 14:15:29 +0300

    Commit

--- app/src/main/java/com/teampay/teampay/MainActivity.java
+++ app/src/main/java/com/teampay/teampay/MainActivity.java
@@ -1,5 +1,11 @@
 package com.teampay.teampay;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,10 +14,64 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    private final IntentFilter intentFilter = new IntentFilter();
+    WifiP2pManager.Channel channel;
+    WifiP2pManager manager;
+    boolean isP2pEnabled = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //  Indicates a change in the Wi-Fi P2P status.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+
+        // Indicates a change in the list of available peers.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+
+        // Indicates the state of Wi-Fi P2P connectivity has changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+
+        // Indicates this device's details have changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        channel = manager.initialize(this, getMainLooper(), null);
+
+        BroadcastReceiver receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+                    // Determine if Wifi P2P mode is enabled or not, alert
+                    // the Activity.
+                    int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+                    if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                        isP2pEnabled = true;
+                    } else {
+                        isP2pEnabled = false;
+                    }
+                } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+
+                    // The peer list has changed!  We should probably do something about
+                    // that.
+
+                } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+
+                    // Connection state changed!  We should probably do something about
+                    // that.
+
+                } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {/*
+                    DeviceListFragment fragment = (DeviceListFragment) activity.getFragmentManager()
+                            .findFragmentById(R.id.frag_list);
+                    fragment.updateThisDevice((WifiP2pDevice) intent.getParcelableExtra(
+                            WifiP2pManager.EXTRA_WIFI_P2P_DEVICE));*/
+
+                }
+            }
+        };
+
     }
 
 
